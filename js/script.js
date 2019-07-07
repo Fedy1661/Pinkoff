909bf3e04f44b16f6d73f955194e93a06ce7b3ca
BuildTools <unconfigured@null.spigotmc.org>
2019-07-07 19:18:19 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,12 @@ function randomPreloader () { // Рандомный прелоадер при п
 $('#svgPreloader').attr('data',randomPreloader())
 
 var preloader = document.getElementById("preloader_preload");
-function fadeOutnojquery(el){el.style.opacity = 1;
+function fadeOutnojquery(el){
+  el.style.opacity = 1;
   var interpreloader = setInterval(function(){
     el.style.opacity = el.style.opacity - 0.05;
-    if (el.style.opacity <=0.05){ clearInterval(interpreloader);
+    if (el.style.opacity <=0.05){ 
+      clearInterval(interpreloader);
       preloader.style.display = "none";
     }},16);
   };
