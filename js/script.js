26d15087b1680babc8e4cb52f2eea5b526ca7175
BuildTools <unconfigured@null.spigotmc.org>
2019-07-07 21:29:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ function fadeOutnojquery(el){
       clearInterval(interpreloader);
       preloader.style.display = "none";
     }},16);
-  $('.tabs').hide().fadeIn(1300);
-  $('.tabs-content').hide().fadeIn(2000, 'linear');
+  $('.tabs').fadeIn(1300);
+  $('.tabs-content').fadeIn(2000, 'linear');
   };
 $(document).ready(function () {
   setTimeout(function(){
